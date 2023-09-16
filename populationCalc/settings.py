cde11920ca285e0166020287f2d23572c9a0e226
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-09-16 11:34:19 -0600

    config vercel

--- populationCalc/settings.py
+++ populationCalc/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://population-calc-front-vercel.vercel.app",
-    "https://population-calc-front.vercel.app/"
+    "https://population-calc-front.vercel.app"
 ]
 
 
