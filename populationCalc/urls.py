a10849fbcf9683722d75a36e526b44bb58b633a7
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-09-15 19:26:19 -0600

    config app to vercel

--- populationCalc/urls.py
+++ populationCalc/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('calcapi.urls'))
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
